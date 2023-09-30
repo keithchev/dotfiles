31e1081119120571e49f33673a76925735080f10
Keith Cheveralls <keith.chev@gmail.com>
2023-09-30 10:07:02 -0700

    add system prompts to llm calls (rather than relying on predefined local `llm` templates)

--- llm/aliases.zsh
+++ llm/aliases.zsh
@@ -1,7 +1,14 @@
+# assume that `llm` is installed in the base conda env
+local LLM="$HOME/opt/miniconda3/bin/llm"
+
+alias llm="$LLM"
+
 function asc () {
-    llm -t cmd-only "$@"
+    local CMD_ONLY="reply with macos terminal commands only. do not include any extra information."
+    "$LLM" -s $CMD_ONLY "$@"
 }
 
 function ask () {
-    llm -t eng-only "$@"
+    local ENG_ONLY="imagine you are speaking to a senior software engineer. reply in succinct, detailed, technical terms. avoid extraneous explanations, qualifiers, and caveats."
+    "$LLM" -s $ENG_ONLY "$@"
 }

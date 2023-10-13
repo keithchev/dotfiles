5afd73f78a9fb0eeadc521c453fe922550ad629e
Keith Cheveralls <keith.chev@gmail.com>
2023-10-13 07:50:22 -0700

    use gpt4 for llm commands, tweak system prompt, add function to eval the output

--- llm/aliases.zsh
+++ llm/aliases.zsh
@@ -3,12 +3,19 @@ local LLM="$HOME/opt/miniconda3/bin/llm"
 
 alias llm="$LLM"
 
+LLM_OUTPUT="echo 'call the asc command first'"
+
 function asc () {
     local CMD_ONLY="reply with macos terminal commands only. do not include any extra information."
-    "$LLM" -s $CMD_ONLY "$@"
+    LLM_OUTPUT=$("$LLM" -s $CMD_ONLY -m gpt4 "$@")
+    echo "$LLM_OUTPUT"
 }
 
 function ask () {
-    local ENG_ONLY="imagine you are speaking to a senior software engineer. reply in succinct, detailed, technical terms. avoid extraneous explanations, qualifiers, and caveats."
-    "$LLM" -s $ENG_ONLY "$@"
+    local ENG_ONLY="imagine you are speaking to a senior software engineer. reply in succinct, detailed, technical terms. avoid verbose explanations and extraneous qualifiers or caveats."
+    "$LLM" -s $ENG_ONLY -m gpt4 "$@"
+}
+
+function evalasc () {
+    eval "$LLM_OUTPUT"
 }

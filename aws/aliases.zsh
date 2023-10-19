6069dd314efffe0b4cf0700a34926f6469d3a034
Keith Cheveralls <keith.chev@gmail.com>
2023-10-19 11:11:41 -0700

    rewrite aws aliases as functions

--- aws/aliases.zsh
+++ aws/aliases.zsh
@@ -1,13 +1,27 @@
 
 alias s3ls='aws s3 ls'
-alias s3ls='aws s3 ls s3://$1'
 
-alias s3cp='aws s3 cp $1 s3://$2'
-alias s3rm='aws s3 rm --dryrun s3://$1'
-alias s3rm-im-sure='aws s3 rm s3://$1'
+function s3lsb () {
+    aws s3 ls --human-readable s3://$1
+}
 
+function s3cp () {
+    aws s3 cp $1 s3://$2
+}
+
+function s3rm () {
+    aws s3 rm --dryrun s3://$1
+}
+
+function s3rm-im-sure () {
+    aws s3 rm s3://$1
+}
+
+# list all ec2 instances as rows of (id, state, name)
 alias ec2ls='aws ec2 describe-instances | \
     jq -r ".Reservations[].Instances[] | \
     (.InstanceId + \" \" + .State.Name + \" \" + ( .Tags[]? | select(.Key == \"Name\") | .Value))"'
 
-alias ec2status='aws ec2 describe-instance-status --instance-id $1'
+function ec2status () {
+    aws ec2 describe-instance-status --instance-id $1
+}

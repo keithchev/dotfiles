c73f711eb895481dc22c89d06fce2b12b401dd39
Keith Cheveralls <keith.chev@gmail.com>
2023-10-18 18:16:43 -0700

    rename some gh aliases

--- gh/aliases.zsh
+++ gh/aliases.zsh
@@ -1,10 +1,15 @@
 alias gh-view='gh repo view --web'
-alias ghv='gh repo view --web'
-
 alias gh-view-pr='gh pr view --web'
-alias ghvp='gh pr view --web'
 
 # open local repos on github
-function kc () {
+function kc-view () {
     gh repo view --web keithchev/"$1"
+}
+
+function ac-view () {
+    gh repo view --web arcadia-science/"$1"
+}
+
+function ac-clone () {
+    gh repo clone arcadia-science/"$1"
 }

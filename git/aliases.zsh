22d485410b370389af6da012bc47fe48f49f2b21
Keith Cheveralls <keith.chev@gmail.com>
2024-03-17 07:45:27 -0700

    remove redundant git aliases and delete the custom glg alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,14 +1,7 @@
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-
-alias gt='git status'
-alias gs='git status -sb'
+# most aliases are defined in the zsh git plugin; these are only custom or overridden aliases.
+alias gt='git status -sb'
 alias gr="git remote -vv"
 alias gb="git branch -a -vv"
 alias gbk="git branch -a -vv | grep kcc"
 
 alias glg='git log --pretty=format:"%h %Cblue %d %Cred %ad %Cgreen %ae %Creset %<(100,trunc) %s" --date=short --graph --all'
-
-# Remove `+` and `-` from start of diff lines; just rely upon color.
-alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'

887336c37400783f319c5f2b786ebc89d8334742
Keith Cheveralls <keith.chev@gmail.com>
2024-06-03 15:57:23 -0700

    switch from exa to eza

--- eza/aliases.zsh
+++ eza/aliases.zsh
@@ -1,24 +1,24 @@
 # standard ls
-alias ll="exa -al --sort=name --time-style=long-iso --group-directories-first"
+alias ll="eza -al --sort=name --time-style=long-iso --group-directories-first"
 
 # only directories
 alias lld="ll -d */"
 
 # sort by file size (largest last)
-alias lls="exa -al --sort=size --time-style=long-iso"
+alias lls="eza -al --sort=size --time-style=long-iso"
 
 # sort by last modified (most recent last)
-alias llt="exa -al --sort=modified --time-style=long-iso"
+alias llt="eza -al --sort=modified --time-style=long-iso"
 
 # only directories, sort by last modified
 alias lltd="llt -d */"
 alias lldt="lltd"
 
 # show tree view of current directory (sub-directories and their contents)
-alias lltree="exa -al --sort=name --tree --level=2"
+alias lltree="eza -al --sort=name --tree --level=2"
 
 # show tree view of with an extra level (sub-sub-directories and their contents)
-alias lltree3="exa -al --sort=name --tree --level=3"
+alias lltree3="eza -al --sort=name --tree --level=3"
 
 # list only files of a certain type
 llf () {

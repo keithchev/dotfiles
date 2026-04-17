5dd9f1c052e6019d974f6c2bccc6def3197706c9
Keith Cheveralls <keith.chev@gmail.com>
2026-04-17 11:38:17 -0700

    add command to set git user

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -5,3 +5,13 @@ alias gb="git branch -a -vv"
 alias gbk="git branch -a -vv | grep kcc"
 alias gdn="git diff --name-only"
 alias glg='git log --pretty=format:"%h %Cblue %d %Cred %ad %Cgreen %ae %Creset %<(100,trunc) %s" --date=short --graph --all'
+
+function git-set-user () {
+    if [[ $# -ne 2 ]]; then
+        echo "Usage: git-set-user <name> <email>"
+        return 1
+    fi
+    git config --file ~/.gitconfig.local user.name "$1"
+    git config --file ~/.gitconfig.local user.email "$2"
+    echo "Set user.name='$1' and user.email='$2' in ~/.gitconfig.local"
+}
